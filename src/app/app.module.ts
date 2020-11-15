fafb3bdc21c9939b81581ec83151f5726c8c8123
jose-acosta-git <jose.acosta.git@gmail.com>
2020-11-15 11:03:57 -0300

    agrega ruteo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { BeerListComponent } from './beer-list/beer-list.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 import { FormsModule } from "@angular/forms";
+import { BreweryBeersComponent } from './brewery-beers/brewery-beers.component';
+import { BreweryAboutComponent } from './brewery-about/brewery-about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BeerListComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    BreweryBeersComponent,
+    BreweryAboutComponent
   ],
   imports: [
     BrowserModule,
